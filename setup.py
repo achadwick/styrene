fa68a82df44663c187534a477826744ae353408a
Andrew Chadwick <a.t.chadwick@gmail.com>
2016-11-26 18:12:30 +0000

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 
 setup(
     name="Styrene",
-    version="0.1.1",
+    version="0.2.0",
     description=(
         "Tool to make usable app bundles for regular Windows users "
         "out of MSYS2 binary packages"
